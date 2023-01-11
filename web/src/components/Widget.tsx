3cd1149294e53cee5798d1f6caf2847c9bca43a9
Gabriel Marques <marquesgabriel@live.com>
2023-01-10 22:24:25 -0300

    Widget accessibility

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,20 +1,14 @@
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
 
-    const [isWidgetOpen, setWidgetOpen] = useState(false)
-
-    function toggleWidgetVisibility() {
-        setWidgetOpen(!isWidgetOpen)
-    }
-
     return (
-        <div className="absolute bottom-5 right-5">
+        <Popover className="absolute bottom-5 right-5">
 
-            {isWidgetOpen &&  <p>"Hello World"</p>}
+            <Popover.Panel>Hello World</Popover.Panel>
 
-            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6" />
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
@@ -24,8 +18,8 @@ export function Widget() {
                     Feedback
                     
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
         
 }
