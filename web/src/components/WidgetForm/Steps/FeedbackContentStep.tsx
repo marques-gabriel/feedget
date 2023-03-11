ea816defabd1d09f72f5f614bbcf90cd8aa9242f
Gabriel Marques <59850744+marques-gabriel@users.noreply.github.com>
2023-03-11 20:34:29 -0300

    feat(feedback content): add feedback form

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -33,10 +33,25 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-               
+            <form className="my-4 w-full">
+                <textarea 
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 foucus:outline-none focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Informe com detalhes o que está acontecendo ... "
+                
+                />
 
-            </div>
+
+                <footer className="flex gap-2 mt-2">
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colorss"
+                    
+                    >
+                        Enviar feedback
+                    </button>
+                </footer>
+
+            </form>
         </>
 
     )
