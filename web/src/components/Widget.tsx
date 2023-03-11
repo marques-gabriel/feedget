19a71e9cd57c2399d7c61478c5ab7acd5894eca8
Gabriel Marques <59850744+marques-gabriel@users.noreply.github.com>
2023-03-11 17:00:49 -0300

    refactor: refactor widget component

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -8,7 +8,7 @@ export function Widget() {
         <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
 
             <Popover.Panel>
-                <WidgetForm></WidgetForm>
+                <WidgetForm />
             </Popover.Panel>
 
             <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
